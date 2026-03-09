71e948c2a4b3ccf6ad22e2a50f8d5e728f20291d
Akash Ajay Malvi <akashmalvi@gmail.com>
2026-03-09 16:34:07 +0530

    main

---
+++ backend/src/utils/pdfParser.js
@@ -0,0 +1,12 @@
+import pdf from "pdf-parse"
+import fs from "fs"
+
+export const extractText = async (filePath) => {
+
+const dataBuffer = fs.readFileSync(filePath)
+
+const data = await pdf(dataBuffer)
+
+return data.text
+
+}
